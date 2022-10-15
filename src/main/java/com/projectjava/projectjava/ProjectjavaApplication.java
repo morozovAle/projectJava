44bda467f761910eaedca05ea2111f4f1b7650aa
MikiSanya <115845131+MikiSanya@users.noreply.github.com>
2022-10-15 05:27:19 +0300

    first step

---
+++ src/main/java/com/projectjava/projectjava/ProjectjavaApplication.java
@@ -0,0 +1,14 @@
+package com.projectjava.projectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjavaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectjava/projectjava/ProjectjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjava.projectjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
